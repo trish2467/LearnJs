9dfccf5e415715abc7b91a2b665687a77ec64ca5
trish2467 <126043505+trish2467@users.noreply.github.com>
2025-01-14 16:02:58 +0000

    conversion

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,14 @@
+let score="33abc" 
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber=Number(score);//changed string to no
+console.log(typeof ValueInNumber);//type of variable
+console.log(ValueInNumber);//value->NaN(output) in undefined and "33abc"
+                          //0 in null->type--?object
+//"33"-->33
+//"33abc"-->NaN
+//TRUE-->1 FALSE-->0
+//name->boolean->true
+//""->boolean-->false
