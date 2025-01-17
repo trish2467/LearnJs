ae32ededc422d952218791c45f173bdda1c51fe9
trish2467 <126043505+trish2467@users.noreply.github.com>
2025-01-17 18:26:16 +0000

    datatypes

---
+++ 01_basics/4_datatypesSummary.js
@@ -0,0 +1,36 @@
+/* dtatypes ->primitive or non primitive
+//primitive
+1->String ,Number,Boolean,null,undefined,Symbol(to make value unique),BigInt
+
+//reference type or Non primitive(directly allocate refernce in memory)
+2->arrays,objects,functions
+ 
+Symbol()->specifies unique
+const id=Sysmbol('123')
+---ARRAY----
+const heros=["shinchan","barbie","nobita"];
+
+----OBJECT-----
+objects are always wriiten in block
+let myObj{
+ name:"trish ",
+ age:21;
+}
+
+--FUNCTION---
+can be treated as a variable
+
+const myFunction=function()
+{
+write here
+}
+
+
+----imp vlues--
+undefined->undefined
+null->object
+arrays->object
+function->object function
+
+
+*/
