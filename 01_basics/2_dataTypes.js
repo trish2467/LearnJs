a28861e3d5315b4369a5056fd37af5da5c947286
trish2467 <126043505+trish2467@users.noreply.github.com>
2025-01-18 11:05:41 +0000

    datatypes

---
+++ 01_basics/2_dataTypes.js
@@ -0,0 +1,12 @@
+ "use strict;" //treat all js code as newer version
+
+ //alert(3+3)->dont occur in node js ,occurr in browser
+
+ let name="trish" //string
+ let age=21 //int 
+ let isDone=false //boolean
+ //null->khali value not 0
+ //undefined->not defined
+ //symbol->unique
+
+console.log(typeof "trish");
